a07942e86dd14ca708fe340213f9ffd54f987ea3
ranichandnirani <chandnirani229@gmail.com>
2026-02-24 18:03:19 +0530

    Some operations using operators

---
+++ L01 Basics Of Python/001 Operators/Arithmetic Operators/01_Calculator.py
@@ -0,0 +1,12 @@
+# 1. Simple Calculator Program
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+print(f"\nAddition:     {a + b}")
+print(f"Subtraction:    {a - b}")
+print(f"Multiplication: {a * b}")
+print(f"Division:       {a / b}")
+print(f"Floor Division: {a // b}")
+print(f"Modulus:        {a % b}")
+print(f"Exponentiation: {a ** b}")
---
+++ L01 Basics Of Python/001 Operators/Arithmetic Operators/02_Area-and-Peramiter.py
@@ -0,0 +1,9 @@
+# 2. Area & Perimeter (Practical Use)
+
+length = 10
+width = 5
+
+area      = length * width        # Multiplication
+perimeter = 2 * (length + width)  # Addition
+print("Area:     ", area)
+print("Perimeter:", perimeter)
