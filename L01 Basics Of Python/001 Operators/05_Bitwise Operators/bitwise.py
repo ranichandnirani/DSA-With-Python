fef78111056785a89a7db079b739c066001cc1cf
ranichandnirani <chandnirani229@gmail.com>
2026-02-27 18:44:25 +0530

    Bitwise operators

---
+++ L01 Basics Of Python/001 Operators/05_Bitwise Operators/bitwise.py
@@ -0,0 +1,11 @@
+# All Bitwise Operators 
+
+a = 5   # binary: 0101
+b = 3   # binary: 0011
+
+print("a & b  =", a & b)    # 1  → 0001
+print("a | b  =", a | b)    # 7  → 0111
+print("a ^ b  =", a ^ b)    # 6  → 0110
+print("~a     =", ~a)       # -6
+print("a << 1 =", a << 1)   # 10 → 1010
+print("a >> 1 =", a >> 1)   # 2  → 0010
