4493376e076788ef8a6df141d0adc39df35af6ee
ranichandnirani <chandnirani229@gmail.com>
2026-03-05 17:00:25 +0530

    Numaric data type (complex datatype)

---
+++ L01 Basics Of Python/002 Data Types/01 Numaric Data Type/03_complex.py
@@ -0,0 +1,11 @@
+# complex — Complex Numbers
+# Numbers with real and imaginary parts.
+
+a = 2 + 3j
+b = 5 - 4j
+
+print(a.real)    # 2.0
+print(a.imag)    # 3.0
+print(a + b)     # (7-1j)
+print(a * b)     # (22+7j)
+print(type(a))   # <class 'complex'>
